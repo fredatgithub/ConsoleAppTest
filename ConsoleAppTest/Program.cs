6e98bd77c344bc78fde556f61930b823e3984a3c
fred <domaine75@gmail.com>
2022-11-11 17:34:50 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleAppTest
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display($"The Framework is: {RuntimeInformation.FrameworkDescription}");
+      display(string.Empty);
+      display($"The Process Architecture is: {RuntimeInformation.ProcessArchitecture}");
+      display(string.Empty);
+      display($"The OS Architecture is: {RuntimeInformation.OSArchitecture}");
+      display(string.Empty);
+      display($"The OS Description is: {RuntimeInformation.OSDescription}");
+      display(string.Empty);
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
